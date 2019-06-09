9afbf206e3aa96c96c2982cb8a8f3af3779f0ff2
santoshvijapure <vijapuresantosh@gmail.com>
2019-06-09 13:18:55 +0530

    dark functionality added

--- content.js
+++ content.js
@@ -1,2 +1,8 @@
-console.log(
-);
+(function () {
+
+    var link = document.createElement("link");
+    link.href = chrome.extension.getURL("style.css");
+    link.type = "text/css";
+    link.rel = "stylesheet";
+    document.getElementsByTagName("head")[0].appendChild(link);    
+})();
